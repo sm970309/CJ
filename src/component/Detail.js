a642c4f55db749c0ce231072d875767d0571abc6
sm970309 <sm970309@gmail.com>
2024-07-06 02:02:17 +0900

    이미지추가+상세페이지 구현

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -7,24 +7,55 @@ const Detail = () => {
   const queryParams = new URLSearchParams(location.search);
   const imageNumber = queryParams.get("image");
 
+  const getImageTitle = (imageNumber) => {
+    switch (parseInt(imageNumber)) {
+      case 1:
+        return "아이스브레이킹";
+      case 2:
+        return "찬양 율동";
+      case 3:
+        return "레크레이션";
+      case 4:
+        return "실내 부스활동";
+      case 5:
+        return "실외 부스활동";
+      case 6:
+        return "연극";
+      case 7:
+        return "3일차 예배";
+      case 8:
+        return "물놀이";
+      case 9:
+        return "간식 만들기";
+      case 10:
+        return "살미 수요 예배";
+      default:
+        return "알 수 없는 프로그램 번호입니다.";
+    }
+  };
+
   const getImageContent = (imageNumber) => {
-    switch (imageNumber) {
-      case "1":
-        return "프로그램 1에 대한 내용";
-      case "2":
-        return "프로그램 2에 대한 내용";
-      case "3":
-        return "프로그램 3에 대한 내용";
-      case "4":
-        return "프로그램 4에 대한 내용";
-      case "5":
-        return "프로그램 5에 대한 내용";
-      case "6":
-        return "프로그램 6에 대한 내용";
-      case "7":
-        return "프로그램 7에 대한 내용";
-      case "8":
-        return "프로그램 8에 대한 내용";
+    switch (parseInt(imageNumber)) {
+      case 1:
+        return '"하나님은 000을 사랑해" 큰 종이와 부채로 서로를 소개하고 알아가요.';
+      case 2:
+        return '"목소리로! 표정으로! 온몸으로!" 신나게 찬양해요.';
+      case 3:
+        return '"5살부터 13살까지! 우리는 한 팀!" 하나님 안에서 공동체를 세워가요.';
+      case 4:
+        return '"우리 서로 사랑해" 하나님이 주신 마음을 클레이 소프로 표현해요';
+      case 5:
+        return '"미움 다툼 시기 질투 버리고" 우비에 물감으로 우리의 죄를 적고, 물총으로 우리의 죄를 지워요';
+      case 6:
+        return '"하나님이 나를 엄청 사랑하신대" 선생님들이 직접 연기하고 춤추고 노래하는 연극을 관람해요.';
+      case 7:
+        return '"사랑으로의 초대" 삼삼오오 모여서 복음을 나눠요.';
+      case 8:
+        return '"와 여름이다!!" 더운 여름 계곡에서 신나게 물놀이를 해요.';
+      case 9:
+        return "완벽한 레시피는 없어! 뭘 넣어도 화채는 맛있지!";
+      case 10:
+        return '"아름다운 마음들이 모여서" 살미 순복음 교회의 수요예배를 함께 드려요.';
       default:
         return "알 수 없는 프로그램 번호입니다.";
     }
@@ -38,8 +69,8 @@ const Detail = () => {
   };
   return (
     <div style={{ textAlign: "center", padding: "20px" }}>
-      <h2>프로그램 {imageNumber} 상세 정보</h2>
-      <p>{getImageContent(imageNumber)}</p>
+      <h2>{getImageTitle(imageNumber)}</h2>
+      <p style={{ color: "gray" }}>{getImageContent(imageNumber)}</p>
       <button
         onClick={handleBackClick}
         style={{
--- src/component/Flowing.js
+++ src/component/Flowing.js
@@ -1,10 +1,10 @@
 // src/components/HeartComponent.js
 
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled, { keyframes } from 'styled-components';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../module/firebase';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../module/firebase";
 
 export const Flowing = () => {
   const [loveAmount, setLoveAmount] = useState(0);
@@ -15,7 +15,7 @@ export const Flowing = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const projectRef = doc(db, 'projects', 'projectData');
+        const projectRef = doc(db, "projects", "projectData");
         const docSnap = await getDoc(projectRef);
 
         if (docSnap.exists()) {
@@ -23,14 +23,14 @@ export const Flowing = () => {
           if (fetchedData.Love !== undefined) {
             setLoveAmount(fetchedData.Love);
           } else {
-            console.log('No Love data found!');
+            console.log("No Love data found!");
           }
         } else {
-          console.log('No such document!');
+          console.log("No such document!");
         }
       } catch (error) {
-        console.error('Error fetching data: ', error);
-        setError('데이터를 불러오는데 실패했습니다.');
+        console.error("Error fetching data: ", error);
+        setError("데이터를 불러오는데 실패했습니다.");
       }
     };
 
@@ -53,42 +53,50 @@ export const Flowing = () => {
       case 2:
         return '"목소리로! 표정으로! 온몸으로!" 신나게 찬양해요.';
       case 3:
-        return '"미움 다툼 시기 질투 버리고 우리 서로 사랑해" 하나님 안에서 공동체를 세워가요. ';
+        return '"5살부터 13살까지! 우리는 한 팀!" 하나님 안에서 공동체를 세워가요.';
       case 4:
-        return '"하나님이 나를 엄청 사랑하신대" 선생님들이 직접 연기하고 춤추고 노래하는 연극을 관람해요.';
+        return '"우리 서로 사랑해" 하나님이 주신 마음을 클레이 소프로 표현해요';
       case 5:
-        return '"사랑으로의 초대" 삼삼오오 모여서 복음을 나눠요.';
+        return '"미움 다툼 시기 질투 버리고" 우비에 물감으로 우리의 죄를 적고, 물총으로 우리의 죄를 지워요';
       case 6:
-        return '"와 여름이다!!" 더운 여름 계곡에서 신나게 물놀이를 해요.';
+        return '"하나님이 나를 엄청 사랑하신대" 선생님들이 직접 연기하고 춤추고 노래하는 연극을 관람해요.';
       case 7:
-        return '완벽한 레시피는 없어! 뭘 넣어도 화채는 맛있지!';
+        return '"사랑으로의 초대" 삼삼오오 모여서 복음을 나눠요.';
       case 8:
+        return '"와 여름이다!!" 더운 여름 계곡에서 신나게 물놀이를 해요.';
+      case 9:
+        return "완벽한 레시피는 없어! 뭘 넣어도 화채는 맛있지!";
+      case 10:
         return '"아름다운 마음들이 모여서" 살미 순복음 교회의 수요예배를 함께 드려요.';
       default:
-        return '알 수 없는 프로그램 번호입니다.';
+        return "알 수 없는 프로그램 번호입니다.";
     }
   };
 
   const getImageTitle = (imageNumber) => {
     switch (imageNumber) {
       case 1:
-        return '아이스브레이킹';
+        return "아이스브레이킹";
       case 2:
-        return '찬양 율동';
+        return "찬양 율동";
       case 3:
-        return '레크레이션 및 부스활동';
+        return "레크레이션";
       case 4:
-        return '연극';
+        return "실내 부스활동";
       case 5:
-        return '3일차 예배';
+        return "실외 부스활동";
       case 6:
-        return '물놀이';
+        return "연극";
       case 7:
-        return '간식 만들기';
+        return "3일차 예배";
       case 8:
-        return '살미 수요 예배';
+        return "물놀이";
+      case 9:
+        return "간식 만들기";
+      case 10:
+        return "살미 수요 예배";
       default:
-        return '알 수 없는 프로그램 번호입니다.';
+        return "알 수 없는 프로그램 번호입니다.";
     }
   };
 
@@ -97,7 +105,7 @@ export const Flowing = () => {
   };
 
   return (
-    <div className="component-container" style={{ textAlign: 'center' }}>
+    <div className="component-container" style={{ textAlign: "center" }}>
       <h2>플로잉 현황</h2>
       <HeartContainer>
         <HeartSvg viewBox="-6 0 140 118.4">
@@ -109,7 +117,14 @@ export const Flowing = () => {
               />
             </clipPath>
           </defs>
-          <rect x="0" y={`${118.4 - filledHeight}`} width="128" height={filledHeight} fill="pink" clipPath="url(#heartClip)" />
+          <rect
+            x="0"
+            y={`${118.4 - filledHeight}`}
+            width="128"
+            height={filledHeight}
+            fill="pink"
+            clipPath="url(#heartClip)"
+          />
           <path
             d="M94.4,0c-13.6,0-24.4,10.8-30.4,17.2C58,10.8,47.2,0,33.6,0C14.8,0,0,14.8,0,33.6
             c0,22.8,30.8,43.6,64,72.4C97.2,77.2,128,56.4,128,33.6C128,14.8,113.2,0,94.4,0z"
@@ -121,19 +136,21 @@ export const Flowing = () => {
       </HeartContainer>
       <p>{`현재 금액: ${loveAmount.toLocaleString()} / ${targetAmount.toLocaleString()}`}</p>
 
-      <FlowingListTitle>하나님의 사랑을 100% 전달하는 플로잉 ▼</FlowingListTitle>
+      <FlowingListTitle>
+        하나님의 사랑을 100% 전달하는 플로잉 ▼
+      </FlowingListTitle>
 
       <FlowingList>
-        {Array.from({ length: 8 }).map((_, index) => (
+        {Array.from({ length: 10 }).map((_, index) => (
           <FlowingBox key={index}>
-            <FlowingLabel>카이노스 X 방송팀 응원 지원</FlowingLabel>
+            {/* <FlowingLabel>카이노스 X 방송팀 응원 지원</FlowingLabel> */}
             <FlowingBoxTop>
               <Image
                 src={`${process.env.PUBLIC_URL}/pro${index + 1}.png`}
                 alt={`Programm ${index + 1}`}
                 className="image-item"
                 onClick={() => handleImageClick(index)}
-                style={{ cursor: 'pointer' }}
+                style={{ cursor: "pointer" }}
               />
               <FlowingDesc>{getImageContent(index + 1)}</FlowingDesc>
             </FlowingBoxTop>
